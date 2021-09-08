1543549c5fafa7e065279a6dc6f868f694b23641
alwaki <xalwa999@gmail.com>
2021-09-08 13:40:48 +0200

    added class attributes and method declarations

--- robot_node.h
+++ robot_node.h
@@ -1,6 +1,7 @@
 #pragma once
 
-
+#include <geometry_msgs.h>
+#include <vector.h>
 
 /*!
 * Main class for object instantiation in ROS interface.
@@ -25,14 +26,24 @@ class RobotNode
         ros::NodeHandle nh_;
 
         /*!
-         * Parameters/variables.
+         * Parameters and/or variables.
         */
+        bool leader_flag;
+        geometry_msgs::Pose2D current_pose;
+        std::vector<geometry_msgs::Pose2D> waypoints;
 
         /*!
          * Subscribers, publishers, services.
         */
+        ros::Subscriber waypoint_sub_;
+        ros::Publisher  pose_pub_;
 
         /*!
          * Method declarations.
         */
+        bool init(geometry_msgs::Pose2D initial_pose);
+
+        void waypoint_callback(const geometry_msgs::Pose2D new_map);
+
+        void publish_pose();
 }
