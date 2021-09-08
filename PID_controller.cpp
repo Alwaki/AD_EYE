c10158fa963d20b04426bd9417ca8427106b9d8a
alwaki <xalwa999@gmail.com>
2021-09-08 10:12:53 +0200

    initial controller class

---
+++ PID_controller.cpp
@@ -0,0 +1,26 @@
+#include <cmath>
+#include <vector>
+#include <random>
+
+class Controller
+{
+    public:
+        /*!
+        * Constructor.
+        */
+        Controller();
+
+        /*!
+         * Destructor.
+        */
+        virtual ~Controller();
+
+    private:
+    /*
+    * Attributes
+    */
+
+    /*
+    * Methods
+    */
+}
