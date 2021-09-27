3c85a7adb53aaa3f6a0913d3f80367c776aa8d85
alwaki <xalwa999@gmail.com>
2021-09-27 13:45:39 +0200

    commiting to remove changes later

--- src/robot_node.cpp
+++ src/robot_node.cpp
@@ -1,25 +1,9 @@
-#include "robot_node.h"
-
-RobotNode::RobotNode():
-    nh_("~")
-    {init_node();}
-
-RobotNode::~RobotNode() = default;
-
-bool RobotNode::init_node()
-{
-    waypoint_sub_  = nh_.subscribe<geometry_msgs::Pose2D>("waypoints", 1, &RobotNode::waypoint_callback, this);
-    pose_pub_      = nh_.advertise<geometry_msgs::Pose2D>("pose_output", 100);
-}
-
-void RobotNode::run_node(){}
-
-void RobotNode::waypoint_callback(geometry_msgs::Pose2D point){}
+#include <ros/ros.h>
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "RobotNode");
-    RobotNode robot_node_object;
+    ros::NodeHandle n;
     ros::Rate loop_rate(10);
     while(ros::ok())
     {
